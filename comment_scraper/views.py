8ea327c7d21c56a67dd1279e1991a4a5077e9526
13155283231 <xss13155283231@sina.com>
2017-01-10 21:03:50 +0800

    das

--- comment_scraper/views.py
+++ comment_scraper/views.py
@@ -1,10 +1,14 @@
 from django.shortcuts import render
+from django.template import loader
 from django.http import HttpResponse
 from comment_scraper.sentiment_analy import start_analysis as sa
+import datetime
 # Create your views here.
 def index(request):
-	# v = sa.pos_value(id)
-	return HttpResponse("Hello word!!")
+	now = datetime.datetime.now()
+	template = loader.get_template('comment_scraper/index.html')
+	context = {"str":"hello",'current_date': now}
+	return HttpResponse(template.render(context))
 
 def get_pos(request, stock_id):
 	v = sa.pos_value(int(stock_id))
